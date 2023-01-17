bcac79bc939e271d8f9020e0fb47f7d0a9305139
Simon Bellucci <s.bellucci57@gmail.com>
2023-01-17 01:38:57 +0100

    fix: seo and navigation

--- app/articles/head.tsx
+++ app/articles/head.tsx
@@ -1,13 +1,6 @@
-import { getArticleMetadata } from '@lib/article';
 import { Seo } from '@components/Utility';
 
 const Head = ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
-
-  const article = getArticleMetadata(slug, true);
-
-  if (!article) return null;
-
   return (
     <Seo
       title="Articles"
