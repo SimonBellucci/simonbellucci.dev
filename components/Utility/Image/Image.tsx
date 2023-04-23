f0b7bfeb86eecb800dda2028697e274e605c06b4
Simon Bellucci <40230678+SimonBellucci@users.noreply.github.com>
2023-04-23 20:01:03 +0200

    feat(articles): add new article about og images (#18)

--- components/Utility/Image/Image.tsx
+++ components/Utility/Image/Image.tsx
@@ -3,14 +3,14 @@ import { FunctionComponent } from 'react';
 import { cx } from 'class-variance-authority';
 import { ImageProps } from '@components/Utility/Image/Image.types';
 
-export const Image: FunctionComponent<ImageProps> = ({
-  className,
-  cover,
-  ...props
-}) => {
+export const Image: FunctionComponent<ImageProps> = ({ className, cover, mdx, ...props }) => {
   return (
     <NextImage
-      className={cx(props.fill || cover ? 'object-cover' : '', className)}
+      className={cx(
+        props.fill || cover ? 'object-cover' : '',
+        className,
+        mdx ? 'mx-auto mb-9' : '',
+      )}
       {...props}
     />
   );
