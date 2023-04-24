8d37db372b9e5b55e2d8eb439e04a558dcfa9b5d
Simon Bellucci <s.bellucci57@gmail.com>
2023-04-24 19:21:35 +0200

    fix: word break on CodeInline component

--- components/Content/Code/Inline/Inline.tsx
+++ components/Content/Code/Inline/Inline.tsx
@@ -5,7 +5,7 @@ export const CodeInline: FunctionComponent<ComponentProps<'code'>> = props => {
   return (
     <code
       className={cx([
-        'transitions-colors rounded-lg border border-gray-200 bg-editor-bg-light p-1 text-sm text-editor-fg-light',
+        'transitions-colors break-all rounded-lg border border-gray-200 bg-editor-bg-light p-1 text-sm text-editor-fg-light',
         'dark:border-gray-700 dark:bg-editor-bg-dark dark:text-editor-fg-dark',
       ])}
     >
